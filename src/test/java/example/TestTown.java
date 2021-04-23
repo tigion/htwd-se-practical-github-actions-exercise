e0a0bd62b4c9d5ef0cf5d781ccb688834244e0db
Christoph Zirkelbach <christoph.zirkelbach@htw-dresden.de>
2021-04-23 10:24:20 +0200

    Update JUnit tests

--- src/test/java/example/TestTown.java
+++ src/test/java/example/TestTown.java
@@ -38,60 +38,76 @@ class TestTown {
 	@DisplayName("T01: Add correct town name")
 	@Test
 	void test1_TownName() {
-		// assert 1 - null pointer
-		assertThrows(
-				java.lang.RuntimeException.class,
-				() -> new Town(null, 0),
-				"Expected a RuntimeException (Name is null) - but it did not come.");
+		String message;
+		Town t = new Town(testValue1Name, testValue1Residents);
+
+		// assert 1 - null
+		message = "name is null";
+		assertThrows(IllegalArgumentException.class, () -> new Town(null, 0), message + " (constructor)");
+		assertThrows(IllegalArgumentException.class, () -> t.setName(null), message + " (set)");
 
 		// assert 2 - empty
-		assertThrows(
-				java.lang.RuntimeException.class,
-				() -> new Town("", 0),
-				"Expected a RuntimeException (Name is empty) - but it did not come.");
+		message = "name is empty";
+		assertThrows(IllegalArgumentException.class, () -> new Town("", 0), message + " (constructor)");
+		assertThrows(IllegalArgumentException.class, () -> t.setName(""), message + " (set)");
 
 		// assert 3 - only white spaces
-		assertThrows(
-				java.lang.RuntimeException.class,
-				() -> new Town("  ", 0),
-				"Expected a RuntimeException (Name contains only white spaces) - but it did not come.");
+		message = "name contains only spaces";
+		assertThrows(IllegalArgumentException.class, () -> new Town("  ", 0), message + " (constructor)");
+		assertThrows(IllegalArgumentException.class, () -> t.setName("  "), message + " (set)");
 	}
 
 	@DisplayName("T02: Add correct town residents")
 	@Test
 	void test2_TownResidents() {
+		String message = "residents is less 0";
+
+		// assert 1 - less 0 -> IllegalArgumentException
+		assertThrows(IllegalArgumentException.class, () -> new Town(testValue1Name, -1), message + " (constructor)");
+		assertThrows(IllegalArgumentException.class, () -> {
+			Town t = new Town(testValue1Name, testValue1Residents);
+			t.setResidents(-2);
+		}, message + " (set)");
+
+		/*// assert 1 - less 0 -> Fail
 		Town t = new Town(testValue1Name, -1);
-		assertTrue(t.getResidents() == 0, "Residents less than 0");
+		assertFalse(t.getResidents() < 0, message + " (constructor)");
+		t = new Town(testValue1Name, testValue1Residents);
+		t.setResidents(-2);
+		assertFalse(t.getResidents() < 0, message + " (set)");*/
 	}
 
 	@DisplayName("T03: Get correct town name")
 	@Test
 	void test3_getNameFunction() {
-		// assert 1
+		// assert 1 - constructor equals get
 		Town t = new Town(testValue1Name, testValue1Residents);
-		assertEquals(testValue1Name, t.getName(), "Output not match input");
-		// assert 2
-		t.setName(testValue2Name);
-		assertEquals(testValue2Name, t.getName(), "Output not match input");
+		assertEquals(testValue1Residents, t.getResidents(), "Output not match input");
+
+		// assert 2 - set equals get
+		t.setResidents(testValue2Residents);
+		assertEquals(testValue2Residents, t.getResidents(), "Output not match input");
 	}
 
 	@DisplayName("T04: Get correct town residents")
 	@Test
 	void test4_getResidentsFunction() {
-		// assert 1
+		// assert 1 - constructor equals get
 		Town t = new Town(testValue1Name, testValue1Residents);
-		assertEquals(testValue1Residents, t.getResidents(), "Output not match input");
-		// assert 2
-		t.setResidents(testValue2Residents);
-		assertEquals(testValue2Residents, t.getResidents(), "Output not match input");
+		assertEquals(testValue1Name, t.getName(), "Output not match input");
+
+		// assert 2 - set equals get
+		t.setName(testValue2Name);
+		assertEquals(testValue2Name, t.getName(), "Output not match input");
 	}
 
 	@DisplayName("T05: Get correct town as string")
 	@Test
 	void test5_toStringFunction() {
+		// assert 1 - toString
 		Town t = new Town(testValue1Name, testValue1Residents);
-		String testValue = "Town [name=" + testValue1Name + ", residents=" + testValue1Residents + "]";
-		assertEquals(testValue, t.toString(), "Output not match template");
+		String template = "Town [name=" + testValue1Name + ", residents=" + testValue1Residents + "]";
+		assertEquals(template, t.toString(), "Output not match template");
 	}
 
 }
